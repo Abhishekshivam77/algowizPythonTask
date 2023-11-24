15884a07097e1cd3a23e60fc25c7de76f188db6b
Abhishekshivam77 <shivamkmr37@gmail.com>
2023-11-24 15:13:17 +0530

    git push

--- app.py
+++ app.py
@@ -1,44 +1,62 @@
+import websocket
 import json
 import csv
-import pandas as pd
 from datetime import datetime
-from collections import deque
 
+# Websocket URL
+websocket_url = "wss://functionup.fintarget.in/ws?id=fintarget-functionup"
 
-def calculate_moving_average(data, window_size):
-  
-    pass
+# OLHC data storage
+olhc_data = {"Nifty": [], "Banknifty": [], "Finnifty": []}
 
-async def process_websocket_data():
-    async with websockets.connect("wss://functionup.fintarget.in/ws?id=fintarget-functionup") as websocket:
-        while True:
-            data = await websocket.recv()
-            json_data = json.loads(data)
+# Moving average window
+ma_window = 3
+ma_data = {"Nifty": [], "Banknifty": [], "Finnifty": []}
 
-            olhc_data = calculate_olhc_data(json_data)
+# CSV file path
+csv_file_path = "olhc_data.csv"
 
-            # Store OLHC data in CSV
-            store_olhc_data_in_csv(olhc_data)
+def on_message(ws, message):
+    try:
+        data = json.loads(message)
+        for key, value in data.items():
+            timestamp = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+            olhc_data[key].append([timestamp, value, value, value, value])
+            
+            if len(olhc_data[key]) >= ma_window:
+                # Calculate Simple Moving Average
+                close_prices = [float(item[4]) for item in olhc_data[key][-ma_window:]]
+                ma = sum(close_prices) / ma_window
+                ma_data[key].append([timestamp, ma])
 
-            # Calculate moving average
-            calculate_and_store_moving_average(olhc_data)
+                # Save OLHC data to CSV
+                with open(csv_file_path, 'a', newline='') as csvfile:
+                    csv_writer = csv.writer(csvfile)
+                    csv_writer.writerow([key] + olhc_data[key][-1])
 
+                # Save SMA data to CSV (starting from the 3rd minute)
+                if len(ma_data[key]) >= 3:
+                    with open("sma_data.csv", 'a', newline='') as smafile:
+                        sma_writer = csv.writer(smafile)
+                        sma_writer.writerow([key] + ma_data[key][-1])
 
-def calculate_olhc_data(json_data):
-   
-    pass
+    except Exception as e:
+        print(f"Error processing message: {e}")
 
+def on_error(ws, error):
+    print(f"Error: {error}")
 
-def store_olhc_data_in_csv(olhc_data):
- 
-    pass
+def on_close(ws, close_status_code, close_msg):
+    print("Closed connection")
 
+def on_open(ws):
+    print("Connection opened")
 
-def calculate_and_store_moving_average(olhc_data):
-
-    pass
-
-# Main function to start the WebSocket connection
 if __name__ == "__main__":
-    import asyncio
-    asyncio.get_event_loop().run_until_complete(process_websocket_data())
+    ws = websocket.WebSocketApp(websocket_url,
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
+    ws.on_open = on_open
+
+    ws.run_forever()
